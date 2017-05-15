62fdec728473ec5411babefd27e28b079cc1e8b4
Mathijs Moerman <mathijs.moerman@qrama.io>
2017-05-15 19:23:12 +0200

    update

---
+++ trusty/sensu-client/reactive/sensu-client.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# Copyright (C) 2017  Qrama
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# pylint: disable=c0111,c0103,c0301
+import os
+from subprocess import call
+from charmhelpers.core.templating import render
+from charmhelpers.core.hookenv import status_set, config, open_port, application_version_set, unit_public_ip, local_unit
+from charmhelpers.core.host import service_restart
+from charms.reactive import when, when_not, when_any, set_state
+
+
+CONFIG_DIR = '/etc/sensu/conf.d'
+
+
+@when('apt.installed.sensu')
+@when_not('sensu.monitoring')
+def setup_sensu():
+    rabbitmq = {'host': config()['rabbitmq'].split(':')[0],
+                'port': config()['rabbitmq'].split(':')[1],
+                'password': config()['password']}
+    if not os.path.isdir('/etc/sensu/ssl'):
+        os.mkdir('/etc/sensu/ssl')
+        #todo create ssl files from config
+    render('rabbitmq.json', '{}/rabbitmq.json'.format(CONFIG_DIR), context=rabbitmq)
+    client = {'name': config()['name'], 'public_ip': unit_public_ip(), 'subscriptions': '[\"monitoring\"]'}
+    render('client.json', '{}/client.json'.format(CONFIG_DIR), context=client)
+    render('transport.json', '{}/transport.json'.format(CONFIG_DIR), context={})
+    open_port(3030)
+    application_version_set('0.29')
+    service_restart('sensu-client')
+    status_set('active', 'Sensu-client is active')
+    set_state('sensu.installed')
+
+
+@when('sensu.installed')
+@when_not('sensu.monitoring')
+def install_machine_monitoring():
+    call(['/opt/sensu/embedded/bin/gem', 'install', 'sensu-plugin', '--version', '\'=1.2.0\''])
+    call(['sensu-install', '-p', 'sensu-plugins-load-checks'])
+    call(['sensu-install', '-p', 'sensu-plugins-memory-checks'])
+    call(['sensu-install', '-p', 'sensu-plugins-disk-checks'])
+    render('checks.json', '{}/checks.json'.format(CONFIG_DIR), context={})
+    service_restart('sencu-client')
+    status_set('active', 'Sensu-client is monitoring CPU, memory and disk ')
+    set_state('sensu.monitoring')
